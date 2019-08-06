84831b1ecac117c16b27dc3c2cff93cb9ec6c319
Jake <x7airworker@gmail.com>
2019-08-06 17:07:29 +0200

    Forgot WrapperFactory implementation

--- RopeV2-MC1.14.4/src/main/java/de/ropemc/rv2/mc114/MinecraftWrapperFactoryImpl.java
+++ RopeV2-MC1.14.4/src/main/java/de/ropemc/rv2/mc114/MinecraftWrapperFactoryImpl.java
@@ -4,6 +4,7 @@ import de.ropemc.rv2.api.MinecraftWrapperFactory;
 import de.ropemc.rv2.api.minecraft.util.math.Vec3d;
 import de.ropemc.rv2.api.minecraft.util.math.Vec3i;
 import de.ropemc.rv2.mc114.minecraft.util.math.Vec3dImpl;
+import de.ropemc.rv2.mc114.minecraft.util.math.Vec3iImpl;
 
 public class MinecraftWrapperFactoryImpl implements MinecraftWrapperFactory {
     public Vec3d.Wrapper vec3d(double x, double y, double z) {
@@ -13,18 +14,15 @@ public class MinecraftWrapperFactoryImpl implements MinecraftWrapperFactory {
         return new Vec3dImpl(handle);
     }
 
-    @Override
     public Vec3i.Wrapper vec3i(int x, int y, int z) {
-        return null;
+        return new Vec3iImpl(x, y, z);
     }
 
-    @Override
     public Vec3i.Wrapper vec3i(double x, double y, double z) {
-        return null;
+        return new Vec3iImpl(x, y, z);
     }
 
-    @Override
     public Vec3i.Wrapper vec3i(Object handle) {
-        return null;
+        return new Vec3iImpl(handle);
     }
 }
